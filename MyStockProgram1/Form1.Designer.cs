4f575720eba59a49e9d41180c9eef8e1ea762f36
unanimous0 <unanimous0@naver.com>
2021-12-13 00:23:56 +0900

    Add project files.

---
+++ MyStockProgram1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace MyStockProgram1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axKHOpenAPI1 = new AxKHOpenAPILib.AxKHOpenAPI();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.codeListButton = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.requestButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axKHOpenAPI1
+            // 
+            this.axKHOpenAPI1.Enabled = true;
+            this.axKHOpenAPI1.Location = new System.Drawing.Point(22, 34);
+            this.axKHOpenAPI1.Name = "axKHOpenAPI1";
+            this.axKHOpenAPI1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKHOpenAPI1.OcxState")));
+            this.axKHOpenAPI1.Size = new System.Drawing.Size(101, 50);
+            this.axKHOpenAPI1.TabIndex = 0;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(22, 90);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(117, 52);
+            this.loginButton.TabIndex = 1;
+            this.loginButton.Text = "로그인창";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // codeListButton
+            // 
+            this.codeListButton.Location = new System.Drawing.Point(22, 168);
+            this.codeListButton.Name = "codeListButton";
+            this.codeListButton.Size = new System.Drawing.Size(117, 52);
+            this.codeListButton.TabIndex = 2;
+            this.codeListButton.Text = "종목리스트";
+            this.codeListButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(22, 256);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(117, 28);
+            this.textBox.TabIndex = 3;
+            // 
+            // requestButton
+            // 
+            this.requestButton.Location = new System.Drawing.Point(183, 256);
+            this.requestButton.Name = "requestButton";
+            this.requestButton.Size = new System.Drawing.Size(177, 28);
+            this.requestButton.TabIndex = 4;
+            this.requestButton.Text = "종목정보요청";
+            this.requestButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.requestButton);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.codeListButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.axKHOpenAPI1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI1;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button codeListButton;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button requestButton;
+    }
+}
+
---
+++ MyStockProgram1/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyStockProgram1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Click은 이벤트 속성 (에 +=로 이벤트 메소드 추가)
+            loginButton.Click += LoginButton_Click;
+
+            codeListButton.Click += CodeListButton_Click;
+
+            requestButton.Click += RequestButton_Click;
+
+            // 로그인 시도에 대한 이벤트 속성
+            axKHOpenAPI1.OnEventConnect += API_OnEventConnect;
+
+            // 서버로부터 (요청했던) TR데이터를 전달받았을 때
+            axKHOpenAPI1.OnReceiveTrData += API_OnReceiveTrData;
+        }
+
+        // 로그인 버튼 클릭시 발생하는 이벤트 메소드
+        private void LoginButton_Click(object sender, EventArgs e)   // 이벤트 메소드는 두 가지 파라미터가 필요함
+        {
+            Console.WriteLine("버튼이 눌렸습니다.");
+            axKHOpenAPI1.CommConnect();     // 로그인창 띄우는 명령어
+        }
+
+        // 로그인 시도시 발생하는 이벤트 메소드
+        private void API_OnEventConnect(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnEventConnectEvent e)
+        {
+            Console.WriteLine("사용자가 로그인을 시도했습니다.");
+
+            if (e.nErrCode == 0)
+            {
+                Console.WriteLine("{0} -> 로그인에 성공했습니다.", e.nErrCode);
+                string user_id = axKHOpenAPI1.GetLoginInfo("USER_ID");                      // 사용자 아이디
+                string user_name = axKHOpenAPI1.GetLoginInfo("USER_NAME");                  // 사용자 이름
+                string account_cnt = axKHOpenAPI1.GetLoginInfo("ACCOUNT_CNT");              // 보유계좌수
+                string[] account_list = axKHOpenAPI1.GetLoginInfo("ACCLIST").Split(';');    // 보유계좌의 계좌번호
+                string server_type = axKHOpenAPI1.GetLoginInfo("GetServerGubun");           // 접속서버 구분 (1: 모의투자, 나머지: 실서버)
+
+                Console.WriteLine("사용자 ID\t {0}", user_id);
+                Console.WriteLine("사용자 이름\t {0}", user_name);
+                Console.WriteLine("계좌수\t\t {0}개", account_cnt);
+
+                for (int i = 0; i < Convert.ToInt32(account_cnt); i++)
+                {
+                    Console.WriteLine("계좌번호 {0}\t {1}", i+1, account_list[i]);
+                }
+
+                if (server_type == 1.ToString())
+                {
+
+                    Console.WriteLine("서버 구분\t [{0}] 모의투자 서버", server_type);
+                }
+                else
+                {
+                    Console.WriteLine("서버 구분\t {0} 실서버", server_type);
+                }
+            }
+            else
+            {
+                Console.WriteLine("로그인에 실패했습니다.");
+                switch (e.nErrCode) {
+                    case 100:
+                        Console.WriteLine("에러코드 {0} -> 사용자 정보교환 실패", e.nErrCode);
+                        break;
+                    case 101:
+                        Console.WriteLine("에러코드 {0} -> 서버접속 실패", e.nErrCode);
+                        break;
+                    case 102:
+                        Console.WriteLine("에러코드 {0} -> 버전처리 실패", e.nErrCode);
+                        break;
+                }
+            }
+        }
+
+        // 종목 코드 버튼 클릭시 발생하는 이벤트 메소드
+        private void CodeListButton_Click(object sender, EventArgs e)
+        {
+            /*
+             * [시장구분값] (국내 주식 시장별 종목코드를 ;로 구분해서 전달)
+             * 0 : 장내
+             * 3 : ELW
+             * 4 : 뮤추얼펀드
+             * 5 : 신주인수권
+             * 6 : 리츠
+             * 8 : ETF
+             * 9 : 하이얼펀드
+             * 10 : 코스닥
+             * 30 : K-OTC
+             * 50 : KONEX
+             * NULL : 전체 시장코드
+             */
+
+            // 종목 코드 반환
+            string codeList = axKHOpenAPI1.GetCodeListByMarket(null);
+            string[] codeArray = codeList.Split(';');
+
+            foreach(string code in codeArray)
+            {
+                // 종목코드 입력하면 종목명 주는 API 함수 사용
+                string itemName = axKHOpenAPI1.GetMasterCodeName(code);
+                Console.WriteLine("{0} {1}", code, itemName);
+            }
+
+            Console.WriteLine(codeList);
+        }
+
+        private void RequestButton_Click(object sender, EventArgs e)
+        {
+            string code = textBox.Text;
+            axKHOpenAPI1.SetInputValue("종목코드", code);
+            axKHOpenAPI1.CommRqData("주식정보요청", "opt10001", 0, "1000");
+        }
+
+        private void API_OnReceiveTrData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+        {
+            if (e.sRQName.Equals("주식정보요청"))
+            {
+                string price = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "현재가");
+                string volume = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "거래량");
+                string rangeRate = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "등락율");
+
+                Console.WriteLine("현재가 = {0}", price);
+                Console.WriteLine("거래량 = {0}", volume);
+                Console.WriteLine("등락률 = {0}", rangeRate);
+            }
+        }
+    }
+}
